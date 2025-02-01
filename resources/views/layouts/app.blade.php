fc31016f0f9765896a6841cce292ae00d0605f76
kevinchristianto <christiantok12@gmail.com>
2025-02-01 13:42:19 +0700

    chore: remove laradock, add custom docker compose

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -124,7 +124,7 @@
                 <div class="dropdown">
                     <button class="users arrow-down-icon border border-gray-200 rounded-pill p-4 d-inline-block pe-40 position-relative" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                         <span class="position-relative">
-                            <img src="https://avatar.iran.liara.run/public/49" alt="Image" class="h-32 w-32 rounded-circle">
+                            <img src="{{ asset('assets/images/avatar.png') }}" alt="Image" class="h-32 w-32 rounded-circle">
                             <span class="activation-badge w-8 h-8 position-absolute inset-block-end-0 inset-inline-end-0"></span>
                         </span>
                     </button>
@@ -132,7 +132,7 @@
                         <div class="card border border-gray-100 rounded-12 box-shadow-custom">
                             <div class="card-body">
                                 <div class="flex-align gap-8 mb-20">
-                                    <img src="https://avatar.iran.liara.run/public/49" alt="" class="w-54 h-54 rounded-circle">
+                                    <img src="{{ asset('assets/images/avatar.png') }}" alt="" class="w-54 h-54 rounded-circle">
                                     <div class="">
                                         <h4 class="mb-0">{{ auth()->user()->name }}</h4>
                                         <p class="fw-medium text-13 text-gray-200">{{ auth()->user()->username }}</p>
